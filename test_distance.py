442b82be1162b3ed5ccbf0ad0c26c7708b02d04e
Michael Clement <michael.a.clement@erdc.dren.mil>
2022-11-23 13:08:41 -0600

    added unit tests

---
+++ test_distance.py
@@ -0,0 +1,10 @@
+import unittest
+from src.distance import distance
+
+class TestDistance(unittest.TestCase):
+    def test_distance(self):
+        self.assertEqual(distance('123','122'), 1)
+    def test_distance_bad_input(self):
+        self.assertEqual(distance('1234', '123'), -1)
+
+unittest.main()

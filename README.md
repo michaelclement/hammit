715407b4513c4d22c1e94fdada3f1b10819a91a0
Michael Clement <michael.a.clement@erdc.dren.mil>
2022-11-23 11:33:18 -0600

    removed unnecessary class

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Have you ever wanted to calculate the hamming distance between two strings? Well
 
 ## Usage
 
-`import hammit.distance.distance as ham`
-`ham.Distance().distance('123', '321')`
+`import hammit.distance.distance as hd`
+`hd.distance('123','125')`
+
+output: **1**
--- distance/distance.py
+++ distance/distance.py
@@ -1,22 +1,18 @@
-class Distance():
-    def __init__(self):
-        pass
+def distance(s1: str, s2: str) -> int:
+    """
+    Calculates the hamming distance between two equal length strings.
 
-    def distance(self, s1: str, s2: str) -> int:
-        """
-        Calculates the hamming distance between two equal length strings.
+    s1: string - the first string to test
+    s2: string - the second string to test
 
-        s1: string - the first string to test
-        s2: string - the second string to test
+    returns: int representing hamming distance between s1 and s2
+    """
+    if len(s2) != len(s1):
+        return -1
 
-        returns: int representing hamming distance between s1 and s2
-        """
-        if len(s2) != len(s1):
-            return -1
+    distance = 0
+    for i in range(len(s1)):
+        distance += 1 if s1[i] != s2[i] else 0
 
-        distance = 0
-        for i in range(len(s1)):
-            distance += 1 if s1[i] != s2[i] else 0
-
-        return distance
+    return distance
 
